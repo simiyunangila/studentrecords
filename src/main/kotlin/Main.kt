6332f43a479c4c8df77692a6d39d98668e1ad2e0
student <simiyunangila>
2023-02-03 07:49:56 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    var fullName: String = "Rebecca Simiyu"
+    println(fullName)
+    var age: Byte = 30
+    println(age)
+    var phoneNumber: String= "0720016543"
+    println(phoneNumber)
+    var weightKg: Double = 54.50
+    println(weightKg)
+    var citizen: String = "kenya"
+    println(citizen)
+    var kenyan: Boolean = true
+    println(kenyan)
+
+
+
+
+
+}
